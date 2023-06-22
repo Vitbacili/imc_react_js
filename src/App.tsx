8e8e6b169ea186a2b779966b27aed949b08b212b
Vitbacili <135022354+Vitbacili@users.noreply.github.com>
2023-06-22 09:10:16 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,28 @@ const App = () => {
         </div>
       </header>
 
-      <div className={styles.container}>
+      <div className={styles.container}> 
 
         <div className={styles.leftSide}>
           <h1>Calculo de IMC</h1>
-          <p>O IMC (Índice de Massa Corporal) é reconhecido pela 
-            OMS (Organização Mundial da Saúde) como um padrão 
-            internacional que avalia se as pessoas, entre 20 e 59 
-            anos, estão com peso ideal ou em excesso, em relação a 
-            sua altura. No entanto, vale lembrar que o resultado não 
+          <p>O IMC (Índice de Massa Corporal) é reconhecido pela
+            OMS (Organização Mundial da Saúde) como um padrão
+            internacional que avalia se as pessoas, entre 20 e 59
+            anos, estão com peso ideal ou em excesso, em relação a
+            sua altura. No entanto, vale lembrar que o resultado não
             indica como está o estado nutricional.</p>
-            <input 
+          <input
             type="number"
             placeholder="Informe a sua altura. Ex 1.50"
-            />
+          />
 
-            <input 
+          <input
             type="number"
             placeholder="Informe o seu peso. Ex 50.3"
-            />
-            <button>Calcular</button>
+          />
+          <button>Calcular</button>
 
-        </div> 
+        </div>
 
         <div className={styles.rightSide}>
           ...
@@ -40,7 +40,7 @@ const App = () => {
       </div>
 
     </div>
-  )
-}
+  );
+};
 
 export default App;
